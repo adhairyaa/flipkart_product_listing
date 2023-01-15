c26939107556e2492430a86838af5c81721c9c9f
Dhairya <dhairyagulati370@gmail.com>
2023-01-15 19:52:50 +0530

    Add : State Context

--- src/context/StateProvider.js
+++ src/context/StateProvider.js
@@ -1,8 +1,31 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useReducer } from "react";
 
 const stateContext = createContext();
+const handleDispatch = (state, action) => {
+  switch (action.type) {
+    case "Low To High":
+      return (state = {
+        ...state,
+        sortBy: "Low To High",
+      });
+    case "High to Low":
+      return (state = { ...state, sortBy: "High to Low" });
+    default:
+      return state;
+  }
+};
 export function StateProvider({ children }) {
-  <stateContext.Provider>{children}</stateContext.Provider>;
+  const [state, dispatch] = useReducer(handleDispatch, {
+    sortBy: null,
+    gender: null,
+    size: null,
+  });
+
+  return (
+    <stateContext.Provider value={{ state, dispatch }}>
+      {children}
+    </stateContext.Provider>
+  );
 }
 
 const useStateProvider = () => useContext(stateContext);
--- src/pages/productListing/ProductListing.jsx
+++ src/pages/productListing/ProductListing.jsx
@@ -4,14 +4,26 @@ import Navbar from "../../components/navbar/Navbar";
 import products from "../../products/products.json";
 import "./ProductListing.css";
 function ProductListing() {
-  const sortproducts
+  const getSortedProducts = (products, sortBy) => {
+    return sortBy === "Low To High"
+      ? products.sort((a, b) => a.price - b.price)
+      : sortBy === "High To Low"
+      ? products.sort((a, b) => b.price - a.price)
+      : products;
+  };
   return (
     <div>
       <Navbar />
       <Filter />
       <div className="productListing">
-        products products products products products products products products
-        products products products products
+        {products.map((item) => (
+          <div className="productTile">
+            <img src={item["product-image"]} alt="productImage"></img>
+            <p>{item.name}</p>
+            <p>{item.Price}</p>
+            <p>free Delivery</p>
+          </div>
+        ))}
       </div>
     </div>
   );

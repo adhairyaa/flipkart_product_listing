2e1d27830d388adf3d45585581b0af617359f7eb
Dhairya <dhairyagulati370@gmail.com>
2023-01-15 18:50:32 +0530

    Add : Navbar, Filter Bar

---
+++ src/components/filter/Filter.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Filter.css";
+function Filter() {
+  return (
+    <div className="filterBar">
+      <div className="filterBar-container">
+      <div className="filterBar_title">
+        <h5>Filter</h5>
+        <button>Clear</button>
+      </div>
+      <div className="sort-by-filter">
+        <p>Sort-by</p>
+        <div>
+          <input type="radio" name="sort-by"></input> low-to-high
+        </div>
+        <div>
+          <input type="radio" name="sort-by"></input> high-to-low
+        </div>
+      </div>
+      <div className="gender-filter">
+        <p>Gender</p>
+        <input type="checkbox"></input>Men
+        <input type="checkbox"></input>Women
+      </div>
+      <div className="size-filter">
+        <p>Size</p>
+        <input type="checkbox"></input>L<input type="checkbox"></input>XL
+        <input type="checkbox"></input>S<input type="checkbox"></input>M
+      </div>
+      <div className="brand-filter">
+        <p>Size</p>
+        <input type="checkbox"></input>L<input type="checkbox"></input>XL
+        <input type="checkbox"></input>S<input type="checkbox"></input>M
+      </div>
+      </div>
+    </div>
+  );
+}
+
+export default Filter;
--- src/pages/productListing/ProductListing.jsx
+++ src/pages/productListing/ProductListing.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Filter from "../../components/filter/Filter";
+import Navbar from "../../components/navbar/Navbar";
 import products from "../../products/products.json";
+import "./ProductListing.css";
 function ProductListing() {
-  return <div>ProductListing</div>;
+  const sortproducts
+  return (
+    <div>
+      <Navbar />
+      <Filter />
+      <div className="productListing">
+        products products products products products products products products
+        products products products products
+      </div>
+    </div>
+  );
 }
 
 export default ProductListing;

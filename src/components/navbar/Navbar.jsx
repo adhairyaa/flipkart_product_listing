dab4a98cdcca1b193ea955a5c661f3ec69b0deca
Dhairya <dhairyagulati370@gmail.com>
2023-01-15 20:29:51 +0530

    Minor CSS Fix

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import "./Navbar.css";
 function Navbar() {
-  return <div className="navbar">Flipkart</div>;
+  return (
+    <div className="navbar">
+      Flipkart
+      <span style={{ color: "yellow", fontSize: ".8rem", marginLeft: 0 }}>
+        plus
+      </span>
+      <input placeholder="search....."></input>
+    </div>
+  );
 }
 
 export default Navbar;
